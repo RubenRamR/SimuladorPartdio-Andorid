6f78472f3588140ebbaba6fc1aa8e2e9bad02916
JorgeCuevasGastelum <jorge.cuevas252274@potros.itson.edu.mx>
2026-03-19 15:49:07 -0700

    Avance ViewModel

--- SimulacionPartido/app/src/main/java/ramirez/ruben/simulacionpartido/ui/screens/ListaEquiposScreen.kt
+++ SimulacionPartido/app/src/main/java/ramirez/ruben/simulacionpartido/ui/screens/ListaEquiposScreen.kt
@@ -1,8 +1,9 @@
 package ramirez.ruben.simulacionpartido.ui.screens
 
 import androidx.compose.runtime.Composable
+import ramirez.ruben.simulacionpartido.data.models.Equipo
 
 @Composable
-fun ListaEquiposScreen(viewModel: ){
+fun ListaEquiposScreen(viewModel ){
 
 }
